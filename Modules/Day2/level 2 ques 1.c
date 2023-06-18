7028a0b46897a4bd81bf665f0860155655ecea5f
NityaGoel02 <99743384+NityaGoel02@users.noreply.github.com>
2023-06-18 23:49:32 +0530

    Create level 2 ques 1.c

---
+++ Modules/Day2/level 2 ques 1.c
@@ -0,0 +1,33 @@
+level 2 ques 1
+
+#include <stdio.h>
+
+void swapAlternateElements(int arr[], int size) {
+    for (int i = 0; i < size - 1; i += 2) {
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original Array: ");
+    printArray(arr, size);
+
+    swapAlternateElements(arr, size);
+
+    printf("Modified Array: ");
+    printArray(arr, size);
+
+    return 0;
+}
