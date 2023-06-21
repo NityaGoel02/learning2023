b26af80b171caf2c432db7f09bbde57ea07fd64b
NityaGoel02 <99743384+NityaGoel02@users.noreply.github.com>
2023-06-21 23:45:38 +0530

    Create level 1 ques 2.c

---
+++ Modules/Day 4/level 1 ques 2.c
@@ -0,0 +1,19 @@
+level 1 ques 2
+
+#include <stdio.h>
+
+int main() {
+    int arr[] = {64, 84, 20, 36, 12, 90, 72, 10, 45, 56 };
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    int min = arr[0]; 
+    int max = arr[0]; 
+
+    for (int i = 1; i < len; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
